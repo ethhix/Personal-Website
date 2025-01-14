ec61a329b0e2e9ba3811fff5494e16b7680e5acc
Nick <nicholasterehin@gmail.com>
2025-01-13 23:31:41 -0500

    Header anchors are now highlighted depending on the page the site visiter is currently viewing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import ScrambleAnchor from "../ScrambleAnchor";
+import { useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+  const currentPage = location.pathname;
   return (
     <header className="flex flex-wrap gap-x-6 text-slate-200">
       <ScrambleAnchor
         text="[ethix.dev]"
         to="/"
-        className={
-          "transition duration-500 hover:bg-slate-200 hover:text-black"
-        }
+        className={`transition duration-500 ${
+          currentPage === "/"
+            ? "bg-slate-200 text-black"
+            : "hover:bg-slate-200 hover:text-black"
+        }`}
       />
       <ScrambleAnchor
         text="[experience]"
         to="/experience"
-        className={
-          "transition duration-500 hover:bg-slate-200 hover:text-black"
-        }
+        className={`transition duration-500 ${
+          currentPage === "/experience"
+            ? "bg-slate-200 text-black"
+            : "hover:bg-slate-200 hover:text-black"
+        }`}
       />
       <ScrambleAnchor
         text="[projects]"
         to="/projects"
-        className={
-          "transition duration-500 hover:bg-slate-200 hover:text-black"
-        }
+        className={`transition duration-500 ${
+          currentPage === "/projects"
+            ? "bg-slate-200 text-black"
+            : "hover:bg-slate-200 hover:text-black"
+        }`}
       />
     </header>
   );
